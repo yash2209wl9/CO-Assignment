2870f3c305ce2a603ec3b823e50f79c369007dcc
parthkumar-dev <parthkumar0210@gmail.com>
2026-03-06 01:40:37 +0530

    update and completed btype instructions

--- SimpleAssembler/Btype.py
+++ SimpleAssembler/Btype.py
@@ -1,4 +1,5 @@
 def to_signed_binary(value, bits):
+    value = int(value)
     if value < 0:
         value = (1 << bits) + value
     return format(value, f'0{bits}b')
@@ -14,44 +15,29 @@ funct3_dict = {
     "bgeu": "111"
  }
 
-registers = {
-    "x0":"00000","x1":"00001","x2":"00010","x3":"00011",
-    "x4":"00100","x5":"00101","x6":"00110","x7":"00111",
-    "x8":"01000","x9":"01001","x10":"01010","x11":"01011",
-    "x12":"01100","x13":"01101","x14":"01110","x15":"01111",
-    "x16":"10000","x17":"10001","x18":"10010","x19":"10011",
-    "x20":"10100","x21":"10101","x22":"10110","x23":"10111",
-    "x24":"11000","x25":"11001","x26":"11010","x27":"11011",
-    "x28":"11100","x29":"11101","x30":"11110","x31":"11111"
-    }
-def Btype(pieces, label_dict, pc):
+registers = {"zero":"00000","ra":"00001","sp":"00010","gp":"00011","tp":"00100",
+             "t0":"00101","t1":"00110", "t2":"00111","s0":"01000","s1":"01001",
+             "a0":"01010","a1":"01011","a2":"01100","a3":"01101","a4":"01110",
+             "a5":"01111","a6":"10000","a7":"10001","s2":"10010","s3":"10011",
+             "s4":"10100","s5":"10101","s6":"10110","s7":"10111","s8":"11000",
+             "s9":"11001","s10":"11010","s11":"11011","t3":"11100","t4":"11101",
+             "t5":"11110","t6":"11111"}
+
+def Btype(pieces):
 
     inst = pieces[0]
     rs1 = pieces[1]
     rs2 = pieces[2]
-    label = pieces[3]
 
-    target_pc = label_dict[label]
-    offset = target_pc - pc
-
-    imm_bin = to_signed_binary(offset, 13)
+    imm_bin = to_signed_binary(pieces[3], 13)
 
     imm12 = imm_bin[0]
     imm10_5 = imm_bin[1:7]
     imm4_1 = imm_bin[7:11]
     imm11 = imm_bin[11]
 
-    binary = (
-        imm12 +
-        imm10_5 +
-        registers[rs2] +
-        registers[rs1] +
-        funct3_dict[inst] +
-        imm4_1 +
-        imm11 +
-        opcode
-    )
+  
 
 
  
-    return binary
+    return imm12 +imm10_5 +registers[rs2]+registers[rs1]+ funct3_dict[inst] +imm4_1 +imm11 +opcode
